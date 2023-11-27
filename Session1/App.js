e5f99c8ed357c2c170127f69ab10de7c607d87d4
iamRishabh07 <iamrishabh799@gmail.com>
2023-11-28 03:06:23 +0530

    session 2 code and theroy part added

--- Session1/App.js
+++ Session1/App.js
@@ -15,9 +15,12 @@
  *
  */
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'I am H1 tag'),
+    React.createElement('h1', {}, 'This is Namaste React'),
     React.createElement('h2', {}, 'I am H2 tag'),
   ]),
   React.createElement('div', { id: 'child2' }, [
